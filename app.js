166709c5e914684832b2aadfc6ecd7a9a336710d
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-11-16 11:27:43 -0600

    added comments as a basic skeleton

---
+++ app.js
@@ -0,0 +1,18 @@
+//bring in required things
+
+//Create README.md 
+
+//Ask for title
+//description
+//table of contents optional
+//installation optional
+//usage
+//credits
+//license
+//badges optional
+//features optional
+//contributing optional
+//tests optional
+
+//write array to README.md
+
